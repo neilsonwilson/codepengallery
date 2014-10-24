eea31a1a877986fb37c404b885fc7b92c86cb7b0
Neil <macadamiannut@gmail.com>
2014-10-24 10:18:57 -0700

    added ajax button to temp page, ajax script to javascrip.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -66,6 +66,33 @@ function changePicture (slide) {
 
 
 
+
+
+
+
+function ajax() {
+    var xmlhttp;
+ 
+    xmlhttp = new XMLHttpRequest();
+ 
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 ) {
+           if(xmlhttp.status == 200){
+               document.getElementById("response").innerHTML = xmlhttp.responseText;
+           }
+           else if(xmlhttp.status == 400) {
+              alert('There was an error 400');
+           }
+           else {
+               alert('something else other than 200 was returned');
+           }
+        }
+    }
+ 
+    xmlhttp.open("GET", "/pages/ajax", true);
+    xmlhttp.send();
+}
+
 // document.addEventListener('DOMContentLoaded', function(){
 //   slides = document.getElementsByClassName('catimage');
 //   slides_total = slides.length;
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,4 +8,8 @@ class PagesController < ApplicationController
   def temp
   end
 
+  def ajax
+  	render partial: 'ajax', formats: :html
+  end
+
 end
