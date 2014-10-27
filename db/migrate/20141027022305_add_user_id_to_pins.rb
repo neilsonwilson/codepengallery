c49f7e32b61f145c288b7d55d3be48d8db7f0a43
Neil <macadamiannut@gmail.com>
2014-10-26 21:32:08 -0700

    added pins and users associations

--- db/migrate/20141027022305_add_user_id_to_pins.rb
+++ db/migrate/20141027022305_add_user_id_to_pins.rb
@@ -1,6 +1,6 @@
 class AddUserIdToPins < ActiveRecord::Migration
   def change
     add_column :pins, :user_id, :integer
-    add_column :pins, :index, :string
+    add_index :pins, :user_id
   end
 end
